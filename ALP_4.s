4a9b028b47c99a2425979eaee94d3742dcb974ed
Raza <141234596+KMORaza@users.noreply.github.com>
2024-01-12 21:53:23 +0530

    _

---
+++ ALP_4.s
@@ -0,0 +1,21 @@
+;; Write assembly language program to copy content present in FFAABBCCH memory location to AABBCCDDH memory location.
+            PRESERVE8
+            THUMB
+            AREA    RESET, DATA, READONLY
+            EXPORT  __Vectors
+__Vectors
+            DCD  0x20001000     ; stack pointer value when stack is empty
+            DCD  Reset_Handler  ; reset vector
+            ALIGN
+SRAM_BASE EQU 0x04000000
+            AREA    MYCODE, CODE, READWRITE
+            ENTRY
+          EXPORT Reset_Handler
+Reset_Handler
+;user code starts from next line
+            ; Load data from 'FFAABBCCH' to register R0
+            LDR R0, =0xFFAABBCC
+            LDR R0, [R0]
+            ; Store data from register R0 to 'AABBCCDDH'
+            STR R0, =0xAABBCCDD
+            END
